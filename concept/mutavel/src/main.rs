d56285723123a0eaf406b69ce39337813d3515f1
Carlos daniel <cd38668cd@gmail.com>
2026-09-23 17:14:19 -0300

    Parte nao mutavel

--- concept/mutavel/src/main.rs
+++ concept/mutavel/src/main.rs
@@ -1,4 +1,20 @@
+/*variaveis mutavel
+  variavel mutavel é uma variavel que pode ser alterada durante a execução do programa, ou seja, seu valor pode ser modificado. Para declarar uma variavel mutavel em Rust, utilizamos a palavra-chave "mut" antes do nome da variavel. Por exemplo:
+  
+  EN:
+    Mutable variables
+    A mutable variable is a variable that can be changed during the execution of the program, meaning its value can be modified. To declare a mutable variable in Rust, we use the keyword "mut" before the variable name. For example:
+*/
+
+// Declarando uma funçao main, funçâo principal em rust
 fn main() {
-    println!("Hello, world!");
-    
+    let idade = 25;
+    // Idade e um inteiro de 32 bits aútomatico, mas o valor nao pode ser alterado esta fixado, mas é uma variavel presente na stack
+    idade = 99;
+    // Certamente é um erro 
+    // Ao compilar ou ao tentar compilar será gerando mensagens de erro
+
+    // JEITO CORRETO
+
+     
 }
