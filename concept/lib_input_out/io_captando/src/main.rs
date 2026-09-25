36b6b6639a6f7bc85093c11e48baec91a76aef51
Carlos daniel <cd38668cd@gmail.com>
2026-09-25 20:50:36 -0300

    Tres funcoes e 3 exemplos peculiar de captar entrada de teclado em rust

--- concept/lib_input_out/io_captando/src/main.rs
+++ concept/lib_input_out/io_captando/src/main.rs
@@ -5,17 +5,23 @@ Estou últilizando um arquivo lib
 */
 
 
-use iocaptando::{input, io_i32};
-// Notém este use chamando a lib e suas funçoes 
+use iocaptando::{input, io_i32, io_char};
+// Notem  este "use" chamando a lib e suas funçoes 
 
 
 fn main() {
+    // Funçao que capta a entrada de STRING
     println!("Digite um texto");
     let texto = input();
-
     println!("Texto e {}", texto);
 
+    // Funçao que capta a entrada de numeros INTEIROS
     println!("Digite um numero inteiro");
     let _numero = io_i32();
     println!("Numero e {_numero}");
+
+    // Funçao que capta à entrada de um CHAR
+    println!("Digite um caracter");
+    let caracters = io_char();
+    println!("Seu caracter e {caracters}");
 }
